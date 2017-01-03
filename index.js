bf7567fe8f4ce444b2059ba75ef4e3cc63f912d7
wuotr <woutermartens@gmail.com>
2017-01-03 20:30:00 +0100

    Updated the build-chain further.

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 'use strict';
 
-const Metalsmith = require('metalsmith');
+const metalsmith = require('metalsmith');
 const markdown = require('metalsmith-markdown');
+const sass = require('metalsmith-sass');
 const layouts = require('metalsmith-layouts');
 const watch = require('metalsmith-watch');
 const serve = require('metalsmith-serve');
 
+const devBuild = ((process.env.NODE_ENV || '').trim().toLowerCase() !== 'production');
+
 const meta = require('./metadata.json');
 
-Metalsmith(__dirname)
-  .metadata(meta)                   // Pass our 'metadata' along the build chain.
-  .source('./src')
-  .destination('./build')
-  .clean(true)                      // Clean destination directory before new build?
-  .use(markdown())                  // Use the markdown source file format.
-  .use(layouts({                    // Use the handlebars templating engine.
-      engine: 'handlebars',
-  }))
-  .use(watch({                      // Rebuild static files on change (during development).
-      paths: {
-          'src/**/*': true,
-          'layouts/**/*': '**/*.md',
-      },
-  }))
-  .use(serve({                      // Serve generated static files (during development).
-      port: 8080,
-      verbose: true,
-  }))
-  .build((err) => {                 // Build the static files ;-).
-      if (err) throw err;
-      console.info('Build finished!');
-  });
+metalsmith(__dirname)
+    .clean(!devBuild)                   // Clean destination directory before new build?
+    .source('./src')
+    .destination('./build')
+    .metadata(meta)                     // Pass our 'metadata' along the build chain.
+    .use(markdown())                    // Use the markdown source file format.
+    .use(sass({                         // Use the sass source file format.
+        outputDir: 'css/',
+    }))
+    .use(layouts({                      // Use the handlebars templating engine.
+        engine: 'handlebars',
+        directory: './templates/',
+        default: 'home.html',
+    }))
+    .use(watch({                        // Rebuild static files on change (during development).
+        paths: {
+            './src/**/*': true,
+            './templates/**/*': '**/*.md',
+        },
+    }))
+    .use(serve({                        // Serve generated static files (during development).
+        port: 8080,
+        verbose: true,
+    }))
+    .build((err) => {                   // Build the static files ;-).
+        if (err) throw err;
+        console.info('Build finished!');
+    });
